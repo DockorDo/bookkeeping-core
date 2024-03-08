6700d2491f3c5ec774380ac6f38da63d98715919
v_runzhiguo <v_runzhiguo@tencent.com>
2024-03-08 18:32:13 +0800

    build:remove styled-comp ,add emotion/react

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 // 页面样式初始化
 import 'normalize.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import '@/styles/index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
