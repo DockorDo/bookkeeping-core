3dc9d1516ceef46ec7b54124a4644f75848f6c50
v_runzhiguo <v_runzhiguo@tencent.com>
2024-03-08 20:30:00 +0800

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import styled from '@emotion/styled'
 import Layout from 'layouts/Layout.tsx'
 
-const Container = styled.header`
+const Container = styled.header<{ bgc: string }>`
   width: 100%;
   height: 100vh;
+  background-color: ${prop=>prop.bgc};
 `
 
 function App() {
-  return ( <Container>
+  return ( <Container bgc="yellow">
     <Layout></Layout>
   </Container>)
 }
